57baaeda46c7d49a46144c98adba2e30a4c9efae
Marcin Czaja <62243477+marc3601@users.noreply.github.com>
2022-03-26 13:05:48 +0100

    added key
    key for menu items

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,7 +6,7 @@ const SideBar = ({ bar }) => {
     let offset = bar ? 0 : -240 + "px"
     return (
         <div className='sidebar_container' style={{ left: offset }}>
-            {itemsData.map(item => <MenuItem name={item.name} logo={item.logo} />)}
+            {itemsData.map((item, i) => <MenuItem key={i} name={item.name} logo={item.logo} />)}
         </div>
     )
 }
