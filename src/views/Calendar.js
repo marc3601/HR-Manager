8f416fbea285ab4e449a0cc2f6d95e9090cdfe86
Marcin <marcin.czaja2@gmail.com>
2022-04-10 22:29:23 +0200

    Added key events handlers for calendar navigation

--- src/views/Calendar.js
+++ src/views/Calendar.js
@@ -4,9 +4,23 @@ import ViewHeader from "../components/ViewHeader";
 import arrow_right from "../assets/arrow_right.png";
 import arrow_left from "../assets/arrow_left.png";
 import { months, getCurrentMonth, getCurrentDay } from "../utilities/viewsData";
+import useEventListener from "../utilities/useEventListener";
 const Calendar = () => {
     const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+    const Arrows = ["39", "ArrowRight", "37", "ArrowLeft"];
+    function keyPressHandler({ key }) {
+        if (Arrows.includes(String(key))) {
+            if (key === "ArrowRight") {
+                nextMonth();
+            } else {
+                previousMonth();
+            }
+        }
+    }
+
     const daysArray = new Array(months[currentMonth].noOfDays).fill(0);
+
+    useEventListener("keydown", keyPressHandler);
 
     const renderDaysOff = (initialDay = 1) => {
         let initialDays = [1, 2, 3, 4, 5, 6, 7];
@@ -20,49 +34,48 @@ const Calendar = () => {
         return arr;
     };
 
+    const previousMonth = () => {
+        setCurrentMonth((prev) => {
+            if (currentMonth > 0 && currentMonth <= 11) {
+                return (prev -= 1);
+            } else return prev;
+        });
+    };
+
+    const nextMonth = () => {
+        setCurrentMonth((prev) => {
+            if (currentMonth >= 0 && currentMonth < 11) {
+                return (prev += 1);
+            } else return prev;
+        });
+    };
+
     return (
-        <div className="calendar_container">
+        <div className="calendar_container" onKeyDown={() => console.log("ello")}>
             <ViewHeader title="Kalendarz" buttonTitle="Dodaj zdarzenie" />
             <div className="calendar_main">
                 <div className="calendar">
                     <div className="calendar_month">
-                        <div
-                            onClick={() =>
-                                setCurrentMonth((prev) => {
-                                    if (currentMonth > 0 && currentMonth <= 11) {
-                                        return (prev -= 1);
-                                    } else return prev;
-                                })
-                            }
-                            className="month_left"
-                        >
-                            <img src={arrow_left} alt="arrow_left" width={30} />
-                        </div>
-                        <h2 className="month_name">{months[currentMonth].name}</h2>
-                        <div
-                            onClick={() =>
-                                setCurrentMonth((prev) => {
-                                    if (currentMonth >= 0 && currentMonth < 11) {
-                                        return (prev += 1);
-                                    } else return prev;
-                                })
-                            }
-                            className="month_right"
-                        >
-                            <img src={arrow_right} alt="arrow_right" width={30} />
-                        </div>
-                    </div>
+                        <div onClick={previousMonth} className="month_left">
+                            <img src={arrow_left} alt="arrow_left" width={30} />{" "}
+                        </div>{" "}
+                        <h2 className="month_name"> {months[currentMonth].name} </h2>{" "}
+                        <div onClick={nextMonth} className="month_right">
+                            <img src={arrow_right} alt="arrow_right" width={30} />{" "}
+                        </div>{" "}
+                    </div>{" "}
                     <div className="calendar_grid">
                         <div className="days_names">
-                            <div className="grid_item">Poniedziałek</div>
-                            <div className="grid_item">Wtorek</div>
-                            <div className="grid_item">Środa</div>
-                            <div className="grid_item">Czwartek</div>
-                            <div className="grid_item">Piątek</div>
-                            <div className="grid_item dayoff">Sobota</div>
-                            <div className="grid_item dayoff">Niedziela</div>
-                        </div>
+                            <div className="grid_item"> Poniedziałek </div>{" "}
+                            <div className="grid_item"> Wtorek </div>{" "}
+                            <div className="grid_item"> Środa </div>{" "}
+                            <div className="grid_item"> Czwartek </div>{" "}
+                            <div className="grid_item"> Piątek </div>{" "}
+                            <div className="grid_item dayoff"> Sobota </div>{" "}
+                            <div className="grid_item dayoff"> Niedziela </div>{" "}
+                        </div>{" "}
                         <div className={`days_numbers`}>
+                            {" "}
                             {daysArray.map((item, i) => {
                                 let data = i + 1;
                                 let holidayCheck = renderDaysOff(
@@ -78,14 +91,14 @@ const Calendar = () => {
                                             "today"
                                             }  ${holidayCheck ? "dayoff_number" : ""}`}
                                     >
-                                        {data}
+                                        {data}{" "}
                                     </div>
                                 );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            })}{" "}
+                        </div>{" "}
+                    </div>{" "}
+                </div>{" "}
+            </div>{" "}
         </div>
     );
 };
