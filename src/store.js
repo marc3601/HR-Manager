7334cf2eb078e48fb0aeb723d592e8c05e0ef4b6
Marcin <marcin.czaja2@gmail.com>
2022-04-15 22:58:09 +0200

    Table sort state moved to Redux

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "./features/sideMenuSlice"
+import tableReducer from "./features/tableSortSlice"
 export const store = configureStore({
     reducer: {
         side_menu: menuReducer,
+        table_sort: tableReducer
     },
 })
