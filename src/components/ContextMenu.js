ccc409f9c93dd62a1c935d5148c3e1aebaf26444
Marcin Czaja <marcin.czaja2@gmail.com>
2022-04-22 23:22:03 +0200

    Current month state moved to Redux

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -4,29 +4,30 @@ import "./ContextMenu.scss";
 import useOutsideAlerter from "../utilities/useOutsideAlerter";
 import { hideContextMenu } from "../features/calendarContextSlice";
 const ContextMenu = (props) => {
-    const wrapperRef = useRef();
-    const calendarContext = useSelector((state) => state.calendar_context);
-    let { i, months, currentMonth } = props;
-    useOutsideAlerter(wrapperRef, hideContextMenu);
-    return (
-        <div
-            ref={wrapperRef}
-            style={{
-                left: `${calendarContext.cord.x}px`,
-                top: `${calendarContext.cord.y}px`,
-            }}
-            className="context_menu"
-        >
-            <ul className="context_menu_list">
-                <li>{"Zaznaczona data:"}</li>
-                <li>{`${i + 1} ${months[currentMonth].name}`}</li>
-                <li>Menu option no 2</li>
-                <li>Menu option no 3</li>
-                <li>Menu option no 4</li>
-                <li>Menu option no 5</li>
-            </ul>
-        </div>
-    );
+  const wrapperRef = useRef();
+  const calendarContext = useSelector((state) => state.calendar_context);
+  const currentM = useSelector((state) => state.current_month);
+  let { i, months } = props;
+  useOutsideAlerter(wrapperRef, hideContextMenu);
+  return (
+    <div
+      ref={wrapperRef}
+      style={{
+        left: `${calendarContext.cord.x}px`,
+        top: `${calendarContext.cord.y}px`,
+      }}
+      className="context_menu"
+    >
+      <ul className="context_menu_list">
+        <li>{"Zaznaczona data:"}</li>
+        <li>{`${i + 1} ${months[currentM].name}`}</li>
+        <li>Menu option no 2</li>
+        <li>Menu option no 3</li>
+        <li>Menu option no 4</li>
+        <li>Menu option no 5</li>
+      </ul>
+    </div>
+  );
 };
 
 export default ContextMenu;
--- src/views/Calendar.js
+++ src/views/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Calendar.scss";
 import ViewHeader from "../components/ViewHeader";
@@ -8,128 +8,122 @@ import { months, getCurrentMonth, getCurrentDay } from "../utilities/viewsData";
 import useEventListener from "../utilities/useEventListener";
 import ContextMenu from "../components/ContextMenu";
 import {
-    hideContextMenu,
-    toogleContextMenu,
+  hideContextMenu,
+  toogleContextMenu,
 } from "../features/calendarContextSlice";
+import {
+  setCurrentMonthDown,
+  setCurrentMonthUp,
+} from "../features/currentMonthSlice";
 const Calendar = () => {
-    const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
-    const dispatch = useDispatch();
-    const calendarContext = useSelector((state) => state.calendar_context);
-    const Arrows = ["39", "ArrowRight", "37", "ArrowLeft"];
+  const dispatch = useDispatch();
+  const calendarContext = useSelector((state) => state.calendar_context);
+  const currentM = useSelector((state) => state.current_month);
+  const Arrows = ["39", "ArrowRight", "37", "ArrowLeft"];
 
-    const keyPressHandler = ({ key }) => {
-        if (Arrows.includes(String(key))) {
-            if (key === "ArrowRight") {
-                nextMonth();
-            } else {
-                previousMonth();
-            }
-        }
-    };
+  const keyPressHandler = ({ key }) => {
+    if (Arrows.includes(String(key))) {
+      if (key === "ArrowRight") {
+        nextMonth();
+      } else {
+        previousMonth();
+      }
+    }
+  };
 
-    const daysArray = new Array(months[currentMonth].noOfDays).fill(0);
+  const daysArray = new Array(months[currentM].noOfDays).fill(0);
 
-    useEventListener("keydown", keyPressHandler);
+  useEventListener("keydown", keyPressHandler);
 
-    const renderDaysOff = (initialDay = 1) => {
-        let initialDays = [1, 2, 3, 4, 5, 6, 7];
-        let reversed = [...initialDays].reverse();
-        let initialDayOff = initialDays[reversed.indexOf(initialDay)];
-        let arr = [];
-        for (let i = 0; i < 5; i++) {
-            arr.push(initialDayOff);
-            initialDayOff += 7;
-        }
-        return arr;
-    };
+  const renderDaysOff = (initialDay = 1) => {
+    let initialDays = [1, 2, 3, 4, 5, 6, 7];
+    let reversed = [...initialDays].reverse();
+    let initialDayOff = initialDays[reversed.indexOf(initialDay)];
+    let arr = [];
+    for (let i = 0; i < 5; i++) {
+      arr.push(initialDayOff);
+      initialDayOff += 7;
+    }
+    return arr;
+  };
 
-    const previousMonth = () => {
-        dispatch(hideContextMenu());
-        setCurrentMonth((prev) => {
-            if (currentMonth > 0 && currentMonth <= 11) {
-                return (prev -= 1);
-            } else return prev;
-        });
-    };
+  const previousMonth = () => {
+    dispatch(hideContextMenu());
+    dispatch(setCurrentMonthDown());
+  };
 
-    const nextMonth = () => {
-        dispatch(hideContextMenu());
-        setCurrentMonth((prev) => {
-            if (currentMonth >= 0 && currentMonth < 11) {
-                return (prev += 1);
-            } else return prev;
-        });
-    };
+  const nextMonth = () => {
+    dispatch(hideContextMenu());
+    dispatch(setCurrentMonthUp());
+  };
 
-    const showContextMenu = (e) => {
-        e.preventDefault();
-        let rect = e.target.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-        let id = e.target.id;
-        dispatch(toogleContextMenu({ targetID: id, cord: { x: x, y: y } }));
-    };
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    let id = e.target.id;
+    dispatch(toogleContextMenu({ targetID: id, cord: { x: x, y: y } }));
+  };
 
-    return (
-        <div className="calendar_container" onKeyDown={() => console.log("ello")}>
-            <ViewHeader title="Kalendarz" buttonTitle="Dodaj zdarzenie" />
-            <div className="calendar_main">
-                <div className="calendar">
-                    <div className="calendar_month">
-                        <div onClick={previousMonth} className="month_left">
-                            <img src={arrow_left} alt="arrow_left" width={30} />{" "}
-                        </div>{" "}
-                        <h2 className="month_name"> {months[currentMonth].name} </h2>{" "}
-                        <div onClick={nextMonth} className="month_right">
-                            <img src={arrow_right} alt="arrow_right" width={30} />{" "}
-                        </div>{" "}
-                    </div>{" "}
-                    <div className="calendar_grid">
-                        <div className="days_names">
-                            <div className="grid_item"> Poniedziałek </div>{" "}
-                            <div className="grid_item"> Wtorek </div>{" "}
-                            <div className="grid_item"> Środa </div>{" "}
-                            <div className="grid_item"> Czwartek </div>{" "}
-                            <div className="grid_item"> Piątek </div>{" "}
-                            <div className="grid_item dayoff"> Sobota </div>{" "}
-                            <div className="grid_item dayoff"> Niedziela </div>{" "}
-                        </div>{" "}
-                        <div className={`days_numbers`}>
-                            {" "}
-                            {daysArray.map((item, i) => {
-                                let data = i + 1;
-                                let holidayCheck = renderDaysOff(
-                                    months[currentMonth].intialDay
-                                ).find((element) => element === data);
-
-                                return (
-                                    <div
-                                        onContextMenu={(e) => showContextMenu(e)}
-                                        key={i}
-                                        id={i}
-                                        className={`grid_item_number initial_day_${months[currentMonth].intialDay
-                                            } ${data === getCurrentDay(currentMonth, getCurrentMonth())
-                                                ? "today"
-                                                : ""
-                                            }  ${holidayCheck ? "dayoff_number" : ""}`}
-                                    >
-                                        {data}{" "}
-                                        {calendarContext.show && calendarContext.targetID === i && (
-                                            <ContextMenu
-                                                i={i}
-                                                months={months}
-                                                currentMonth={currentMonth}
-                                            />
-                                        )}
-                                    </div>
-                                );
-                            })}{" "}
-                        </div>{" "}
-                    </div>{" "}
-                </div>{" "}
+  return (
+    <div className="calendar_container" onKeyDown={() => console.log("ello")}>
+      <ViewHeader title="Kalendarz" buttonTitle="Dodaj zdarzenie" />
+      <div className="calendar_main">
+        <div className="calendar">
+          <div className="calendar_month">
+            <div onClick={previousMonth} className="month_left">
+              <img src={arrow_left} alt="arrow_left" width={30} />{" "}
             </div>{" "}
-        </div>
-    );
+            <h2 className="month_name"> {months[currentM].name} </h2>{" "}
+            <div onClick={nextMonth} className="month_right">
+              <img src={arrow_right} alt="arrow_right" width={30} />{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="calendar_grid">
+            <div className="days_names">
+              <div className="grid_item"> Poniedziałek </div>{" "}
+              <div className="grid_item"> Wtorek </div>{" "}
+              <div className="grid_item"> Środa </div>{" "}
+              <div className="grid_item"> Czwartek </div>{" "}
+              <div className="grid_item"> Piątek </div>{" "}
+              <div className="grid_item dayoff"> Sobota </div>{" "}
+              <div className="grid_item dayoff"> Niedziela </div>{" "}
+            </div>{" "}
+            <div className={`days_numbers`}>
+              {" "}
+              {daysArray.map((item, i) => {
+                let data = i + 1;
+                let holidayCheck = renderDaysOff(
+                  months[currentM].intialDay
+                ).find((element) => element === data);
+
+                return (
+                  <div
+                    onContextMenu={(e) => showContextMenu(e)}
+                    key={i}
+                    id={i}
+                    className={`grid_item_number initial_day_${
+                      months[currentM].intialDay
+                    } ${
+                      data === getCurrentDay(currentM, getCurrentMonth())
+                        ? "today"
+                        : ""
+                    }  ${holidayCheck ? "dayoff_number" : ""}`}
+                  >
+                    {data}{" "}
+                    {calendarContext.show && calendarContext.targetID === i && (
+                      <ContextMenu i={i} months={months} />
+                    )}
+                  </div>
+                );
+              })}{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Calendar;
