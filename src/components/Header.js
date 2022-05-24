55447f66edbf7710f98b4752b4fc079f2a6f8490
Marcin Czaja <marcin.czaja2@gmail.com>
2022-05-24 22:09:15 +0200

    Employee add screen - initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { toogle } from "../features/sideMenuSlice";
 import "../components/Header.scss";
 import User from "./User";
 const Header = () => {
+  const menuBar = useSelector((state) => state.side_menu);
+  const dispatch = useDispatch();
   return (
     <header className="main">
       <div className="main_left">
-        <Link className="logo_link" to="/">
+        <Link
+          className="logo_link"
+          to="/"
+          onClick={() => menuBar && dispatch(toogle())}
+        >
           <div className="main_logo">
             <h1>
               Easy HR<sup>&reg;</sup>
